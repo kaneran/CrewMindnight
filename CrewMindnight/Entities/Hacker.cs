2528b5085bac9dcb023c915a2f44632068eaab03
Kane <95705546+kaneran@users.noreply.github.com>
2023-03-02 20:11:36 +0000

    Final logic around if two hackers in the same node, decrease the odds of each hacker hacking

--- CrewMindnight/Entities/Hacker.cs
+++ CrewMindnight/Entities/Hacker.cs
@@ -6,6 +6,7 @@ namespace CrewMindnight.Entities
     public class Hacker : Player, IPlayerAction
     {
         private Dictionary<int, int> _hackerProbabilities;
+        private Random _random;
         public Hacker(string name) : base(name, "Hacker")
         {
             _hackerProbabilities = new Dictionary<int, int>()
@@ -16,25 +17,41 @@ namespace CrewMindnight.Entities
                 {4,95},
                 {5,100}
             };
+            _random = new Random();
         }
-
+        
         public override bool PerformAction(GameProgress gameProgress)
         {
-            //If there are two hacked nodes, hack and win for the hackers! Or... if there are two secured nodes, then the hacker has no choice but to hack..
-            var perfomHack = gameProgress.Audit.Where(node => node.Outcome == "Hacked").Count() == 2 || gameProgress.Audit.Where(node => node.Outcome == "Secured").Count() == 2;
-            if (perfomHack)
+            //If there are two hacked nodes, hack and win for the hackers!
+            if (gameProgress.Audit.Where(node => node.Outcome == "Hacked").Count() == 2)
+            {
+                return false;
+            }
+
+            //If there's two hackers in the node then decrease the likelihood (65%) of them hacking to prevent two hackers detected 
+            else if(gameProgress.Participants.Where(participant => participant.Role == "Hacker").Count() == 2)
+            {
+                return DecideAction(65);
+            }
+
+            //If there are two secured nodes, then the hacker has no choice but to hack..
+            else if(gameProgress.Audit.Where(node => node.Outcome == "Secured").Count() == 2)
             {
                 return false;
             }
             else
             {
-                //Hacker will either hack or secure the node...
-                var random = new Random();
-                var randomNumber = random.Next(1, 100);
-                //Increase the odds of the player, by 10%, hacking the node if player is Shadowbeatz or TheG18 cause they're known to be risky and hack
-                var doHackNode = randomNumber <= _hackerProbabilities[gameProgress.Node] + (this.Name == "ShadowBeatz" || this.Name == "TheG18" ? 10 : 0);
-                return !doHackNode;
+                return DecideAction(_hackerProbabilities[gameProgress.Node]);
             }
+        }
+
+        //Hacker will either hack or secure the node..
+        public bool DecideAction(int probability)
+        {
+            var randomNumber = _random.Next(1, 100);
+            //Increase the odds of the player, by 10%, hacking the node if player is Shadowbeatz or TheG18 cause they're known to be risky and hack
+            var doHackNode = randomNumber <= probability + (this.Name == "ShadowBeatz" || this.Name == "TheG18" ? 10 : 0);
+            return !doHackNode;
         }
     }
 }
