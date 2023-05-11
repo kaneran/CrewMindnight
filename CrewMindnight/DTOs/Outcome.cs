8cc7cec684dc77e9e3f3534d2c8a89eacdc25df6
Kane <95705546+kaneran@users.noreply.github.com>
2023-05-11 18:17:58 +0100

    Resolved JSON Deserialize Error by making Outcome DTO constructor parameter-less

--- CrewMindnight/DTOs/Outcome.cs
+++ CrewMindnight/DTOs/Outcome.cs
@@ -3,18 +3,9 @@
     public class Outcome
     {
         public int Node { get; set; }
-        public int NumberOfHackersDetected { get; }
-        public string Result { get; }
+        public int NumberOfHackersDetected { get; set; }
+        public string Result { get; set; }
 
         public List<string> Participants { get; set; }
-
-
-        public Outcome(int numberOfHackerDetected, string result, int node, List<string> participants)
-        {
-            NumberOfHackersDetected = numberOfHackerDetected;
-            Result = result;
-            Node = node;
-            Participants = participants;
-        }
     }
 }
--- CrewMindnight/Services/MaintenanceService.cs
+++ CrewMindnight/Services/MaintenanceService.cs
@@ -16,7 +16,7 @@ namespace CrewMindnight.Services
             });
             var numberOfHacksDetected = actions.Where(a => !a).Count();
             var result = numberOfHacksDetected > 0 ? "Hacked" : "Secured";
-            return new Outcome(numberOfHacksDetected, result, gameProgress.Node, gameProgress.Participants.Select(p => p.Name).ToList());
+            return new Outcome() {NumberOfHackersDetected= numberOfHacksDetected, Result = result, Node = gameProgress.Node, Participants = gameProgress.Participants.Select(p => p.Name).ToList() };
         }
     }
 }
